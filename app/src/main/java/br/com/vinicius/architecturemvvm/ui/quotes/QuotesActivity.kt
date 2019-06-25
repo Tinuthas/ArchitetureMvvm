189f46103db3da3257f26df4885b7abf5b782ceb
Marcus Medeiros <vinicius1504_medeiros@hotmail.com>
2019-06-25 17:49:32 -0300

    Update layout

--- app/src/main/java/br/com/vinicius/architecturemvvm/ui/quotes/QuotesActivity.kt
+++ app/src/main/java/br/com/vinicius/architecturemvvm/ui/quotes/QuotesActivity.kt
@@ -15,6 +15,7 @@ class QuotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeUI()
     }
 
     private fun initializeUI() {
